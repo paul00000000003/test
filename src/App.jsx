ed435322610e6ed7780ec187d261ecfde743b009
Paul van Mierlo <pauljozefvanmierlo@yahoo.com>
2020-12-06 11:57:28 +0100

    testje

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { karakters: [], schermGeladen: false };
+  }
+
+  render() {
+    return <h1>hoofdstuk 1</h1>;
+  }
+}
+
+export default App;
